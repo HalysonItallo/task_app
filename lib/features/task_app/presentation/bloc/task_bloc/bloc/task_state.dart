ce02131f69c907c152f600eec79edb4cda54ae00
HalysonItallo <halyssonpimentell@gmail.com>
2022-06-27 21:53:45 -0300

    ci: add workflow github actions

--- lib/features/task_app/presentation/bloc/task_bloc/bloc/task_state.dart
+++ lib/features/task_app/presentation/bloc/task_bloc/bloc/task_state.dart
@@ -6,6 +6,6 @@ part 'task_state.freezed.dart';
 @freezed
 class TaskState with _$TaskState {
   const factory TaskState.loading() = _Loading;
-  const factory TaskState.loaded(TaskModel taskModel) = _Loaded;
+  const factory TaskState.loaded(List<TaskModel> listTaskModel) = _Loaded;
   const factory TaskState.empty() = _Empty;
 }
