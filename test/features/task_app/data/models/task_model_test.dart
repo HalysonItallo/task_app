47ab2c1a34340219ac3c7096e02daa07eebc1199
Temistocles Zwang <temis2st@gmail.com>
2022-06-29 13:09:27 -0300

    update: added two tests

--- test/features/task_app/data/models/task_model_test.dart
+++ test/features/task_app/data/models/task_model_test.dart
@@ -6,6 +6,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:task_app/features/task_app/data/models/task_model.dart';
 import 'package:task_app/features/task_app/domain/entities/task_entity.dart';
 
+import '../../../../fixtures/task.json';
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tTaskModel = TaskModel(id: 'tarefa1', description: 'descricao aqui');
@@ -15,4 +17,27 @@ void main() {
     });
   });
 
+  group('json tests', () async {
+    test('should be json', () async {
+      final Map<String, dynamic> jsonMap =
+          jsonDecode(fixture('task.json'));
+
+      final result = TaskModel.fromJson(jsonMap);
+
+      expect(result, tTaskModel);
+    });
+
+    test('should be return a map from json inserted', () async {
+      final result = tTaskModel.toJson();
+      final mapGenerated = {
+        
+        "text": "Test Text",
+        "number": 1,
+        "found": true,
+        "type": "task"
+
+      };
+      expect(result, mapGenerated);
+    });
+  });
 }//.fim
